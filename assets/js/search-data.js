0ca1add8cbf168cca29bdb02659069fafe8f169e
Enderlogic <33103855+Enderlogic@users.noreply.github.com>
2025-08-10 09:13:13 +0000

    Deploying to gh-pages from @ Enderlogic/Enderlogic.github.io@6e7949938604adac14533cbb050c2787dbc6c232 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -56,7 +56,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=GMeLKiQAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
